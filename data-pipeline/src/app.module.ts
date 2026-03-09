65ddf794cbf4a3d2ebd311afda75527d4204cf5c
fzaura <m3alemm0@gmail.com>
2026-03-09 20:10:17 +0300

    feat: generate telemetry controller

--- data-pipeline/src/app.module.ts
+++ data-pipeline/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TelemetryController } from './telemetry/telemetry.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController],
+  controllers: [AppController, TelemetryController],
   providers: [AppService],
 })
 export class AppModule {}
