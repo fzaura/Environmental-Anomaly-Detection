6ad6b5899e8d0b8cccb8d347263a4e0801106482
fzaura <m3alemm0@gmail.com>
2026-03-13 17:45:50 +0300

    feat: generate clean telemetry  module

--- data-pipeline/src/app.module.ts
+++ data-pipeline/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TelemetryController } from './telemetry/telemetry.controller';
+import { TelemetryModule } from './telemetry/telemetry.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TelemetryModule],
   controllers: [AppController, TelemetryController],
   providers: [AppService],
 })
