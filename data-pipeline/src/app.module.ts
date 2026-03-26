87a3f4bb8152b4ef2014a2d4acc39804c1330e72
fzaura <m3alemm0@gmail.com>
2026-03-26 21:26:35 +0300

    feat(app): import configmodule

--- data-pipeline/src/app.module.ts
+++ data-pipeline/src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { TelemetryController } from './telemetry/telemetry.controller';
 import { TelemetryModule } from './telemetry/telemetry.module';
+import { OpenaqModule } from './openaq/openaq.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, TelemetryModule],
-  controllers: [AppController, TelemetryController],
+  imports: [
+    PrismaModule,
+    TelemetryModule,
+    OpenaqModule,
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
