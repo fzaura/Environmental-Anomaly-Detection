a78662e4c85fcd27cecdcf0ecf1bb8e7a8c5f54d
fzaura <m3alemm0@gmail.com>
2026-03-26 22:12:04 +0300

    chore(nodule): fix imports

--- data-pipeline/src/telemetry/telemetry.module.ts
+++ data-pipeline/src/telemetry/telemetry.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TelemetryController } from './telemetry.controller';
 import { TelemetryService } from './telemetry.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [TelemetryController],
   providers: [TelemetryService],
   exports: [TelemetryService],
+  imports: [PrismaModule],
 })
 export class TelemetryModule {}
