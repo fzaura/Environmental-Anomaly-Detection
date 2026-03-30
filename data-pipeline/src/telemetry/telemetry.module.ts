7899664ef88e0158dc86fc29a1ec58606a21e675
fzaura <m3alemm0@gmail.com>
2026-03-31 02:39:55 +0300

    chore(module) import configmodule

--- data-pipeline/src/telemetry/telemetry.module.ts
+++ data-pipeline/src/telemetry/telemetry.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TelemetryController } from './telemetry.controller';
 import { TelemetryService } from './telemetry.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [TelemetryController],
   providers: [TelemetryService],
   exports: [TelemetryService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule],
 })
 export class TelemetryModule {}
